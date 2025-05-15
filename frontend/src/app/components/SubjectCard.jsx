e8e2b151e9933f17280881838d1f995c6fa13eee
altagracia <samirasanchez8090@gmail.com>
2025-05-15 16:33:18 -0400

    backend auth folder and files created as well as users folder

--- frontend/src/app/components/SubjectCard.jsx
+++ frontend/src/app/components/SubjectCard.jsx
@@ -2,20 +2,26 @@ import React from "react";
 
 const SubjectCard = ({ subject, onClose}) => {
     return (
-        <div className="">
-            <button className="" onClick={onClose}>X</button>
-            <img
-                src={`data:image/jpeg;base64,${subject.image}`}
-                alt="Subject"
-                className="w-32 h-32"
-            />
-            <h2 className="">{subject.first_name} {subject.last_name}</h2>
-            <p className="">ID: {subject.id}</p>
-            <p className="">Sex: {subject.sex}</p>
-            <p className="">Street: {subject.address.street}</p>
-            <p className="">City: {subject.address.city}</p>
-            <p className="">State: {subject.address.state}</p>
-            <p className="">Zip: {subject.address.zip_code}</p>
+        <div className="mx-auto my-10 flex-col items-center border px-4 text-center bg-cyberpunk-bg md:max-w-lg md:flex-row mid:items-start md:text-left">
+            <div className="mb-4 md:mr-6 md:mb-0">
+                <button className="" onClick={onClose}>X</button>
+                <img
+                    src={`data:image/jpeg;base64,${subject.image}`}
+                    alt="Subject"
+                    className="h-32 object-cover md:h-48 md:w-32"
+                />
+            </div>
+            <div className="">
+                <p className="text-xl font-medium text-gray-700">{subject.first_name} {subject.last_name}</p>
+                <p className="mb-4 text-sm font-medium text-gray-500">ID: {subject.id}</p>
+                <div className="flex space-x-2 text-left">
+                    <p className="text-sm font-medium text-gray-700">Sex: {subject.sex}</p>
+                    <p className="text-sm font-medium text-gray-700">Street: {subject.address.street}</p>
+                    <p className="text-sm font-medium text-gray-700">City: {subject.address.city}</p>
+                    <p className="text-sm font-medium text-gray-700">State: {subject.address.state}</p>
+                    <p className="text-sm font-medium text-gray-700">Zip: {subject.address.zip_code}</p>
+                </div>
+            </div>
         </div>
     )
 }
